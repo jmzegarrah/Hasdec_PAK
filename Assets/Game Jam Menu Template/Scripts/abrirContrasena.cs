746398a349910116dbfdf133fe718c6fcf65c946
gonzqh <gonzqh@gmail.com>
2017-05-19 19:13:59 -0500

    Fin interfaces login - registro

---
+++ Assets/Game Jam Menu Template/Scripts/abrirContrasena.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abrirContrasena : MonoBehaviour
+{
+    public GameObject Contrasena;
+    // Use this for initialization
+
+    public void abrirContrasenaWindow()
+    {
+        Debug.Log("abrio ventana de contrasena");
+        Contrasena.SetActive(true);
+    }
+
+    public void cancelar()
+    {
+        Contrasena.SetActive(false);
+
+    }
+    public void Guardar()
+    {
+        Contrasena.SetActive(false);
+
+    }
+
+    void Start()
+    {
+        Contrasena.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
---
+++ Assets/Game Jam Menu Template/Scripts/abrirPreguntaSeguridad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abrirPreguntaSeguridad : MonoBehaviour {
+
+
+    public GameObject PreguntadeSeguridad;
+    // Use this for initialization
+
+    public void abrirPregunta()
+    {
+        Debug.Log("Hizo click");
+        PreguntadeSeguridad.SetActive(true);
+    }
+
+    public void cancelar()
+    {
+                  PreguntadeSeguridad.SetActive(false);
+      
+    }
+    public void validar()
+    {
+        Debug.Log("cerro pregunta apra abrir contrasena");
+        PreguntadeSeguridad.SetActive(false);
+
+    }
+
+    void Start () {
+        PreguntadeSeguridad.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
