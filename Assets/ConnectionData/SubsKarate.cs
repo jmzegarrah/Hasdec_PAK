0ed7011d9530c03c7912d6d8313c9c93d9d7705f
DESKTOP-FQ58T21\fred- <fredd.valdivia@gmail.com>
2017-06-19 23:57:11 -0500

    subtitulos tutor

---
+++ Assets/ConnectionData/SubsKarate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class SubsKarate : MonoBehaviour {
+
+    public Image subTutor;
+    //public float DelayTime = 10.0f;
+
+
+
+    // Use this for initialization
+    void Start () {
+        if (LoginController.mostrarSubs == 1)
+        {
+            subTutor.enabled = true;
+        }
+        else {
+            subTutor.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
