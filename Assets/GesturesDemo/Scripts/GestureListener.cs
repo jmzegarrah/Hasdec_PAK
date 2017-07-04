6e88b1eace11905c615cca3cc87759844b734ac4
Ark4n0s <jmzegarrah@gmail.com>
2017-07-04 14:33:18 -0500

    Movimientos

--- Assets/GesturesDemo/Scripts/GestureListener.cs
+++ Assets/GesturesDemo/Scripts/GestureListener.cs
@@ -6,7 +6,15 @@ public class GestureListener : MonoBehaviour, KinectGestures.GestureListenerInte
 {
     // GUI Text to display the gesture messages.
     public GUIText GestureInfo;
-    public int contador =0;
+    public int contador1;
+    public int contador2;
+    public int contador3;
+    public int contador4;
+    public int contador5;
+    public int contador6;
+    public int contador7;
+    public int contador8;
+
     private bool Musibidachi;
     private bool Joe;
     private bool Zenkutsu;
@@ -15,6 +23,99 @@ public class GestureListener : MonoBehaviour, KinectGestures.GestureListenerInte
     private bool OiZuki;
     private bool Uke;
     private bool AgeUke;
+
+    public int MovActual = 0;
+    private string usuarioTemp = LoginController.usuario;
+    public static String modulo = "";
+
+    void Awake()
+    {
+        extraerLockMovimiento(1);
+        extraerLockMovimiento(2);
+        extraerLockMovimiento(3);
+        extraerLockMovimiento(4);
+        extraerLockMovimiento(5);
+        extraerLockMovimiento(6);
+        extraerLockMovimiento(7);
+        extraerLockMovimiento(8);
+    }
+
+    void Start()
+    {
+        SeleccionMovimineto();
+    }
+
+    public void SeleccionMovimineto()
+    {
+        if (MovActual == 1)
+        {
+            Musibidachi = true;
+            contador1 = 0;
+        }
+        else if (MovActual == 2)
+        {
+            Joe = true;
+            contador2 = 0;
+        }
+        else if (MovActual == 3)
+        {
+            Zenkutsu = true;
+            contador3 = 0;
+        }
+        else if (MovActual == 4)
+        {
+            Kokutsu = true;
+            contador4 = 0;
+        }
+        else if (MovActual == 5)
+        {
+            Tsuki = true;
+            contador5 = 0;
+        }
+        else if (MovActual == 6)
+        {
+            OiZuki = true;
+            contador6 = 0;
+        }
+        else if (MovActual == 7)
+        {
+            Uke = true;
+            contador7 = 0;
+        }
+        else if (MovActual == 8)
+        {
+            AgeUke = true;
+            contador8 = 0;
+        }
+
+    }
+
+    public void extraerLockMovimiento(int movimiento)
+    {
+        string url = "http://localhost/PAK_Modulos/PAK_getLockMov.php?" + "UsuSobNom=" + LoginController.usuario + "&NumMov=" + movimiento;
+        StartCoroutine(LockMov(url));
+    }
+
+    IEnumerator LockMov(string url)
+    {
+        Debug.Log(url);
+        WWW conecction = new WWW(url);
+        yield return (conecction);
+        if (conecction.text.Contains("null"))
+        {
+            Debug.Log("No se encuentran datos");
+        }
+        else
+        {
+            if (conecction.text.Contains("1"))
+            {
+                MovActual += 1;
+            }
+        }
+    }
+
+
+
 
     public bool EsMusibidachi()
     {
@@ -109,7 +210,13 @@ public class GestureListener : MonoBehaviour, KinectGestures.GestureListenerInte
         KinectManager manager = KinectManager.Instance;
 
         manager.DetectGesture(userId, KinectGestures.Gestures.Musibidachi);
-        manager.DetectGesture(userId, KinectGestures.Gestures.GolpeIzquierda);
+        manager.DetectGesture(userId, KinectGestures.Gestures.Joe);
+        manager.DetectGesture(userId, KinectGestures.Gestures.Zenkutsu);
+        manager.DetectGesture(userId, KinectGestures.Gestures.Kokutsu);
+        manager.DetectGesture(userId, KinectGestures.Gestures.OiZuki);
+        manager.DetectGesture(userId, KinectGestures.Gestures.Tsuki);
+        manager.DetectGesture(userId, KinectGestures.Gestures.Uke);
+        manager.DetectGesture(userId, KinectGestures.Gestures.AgeUke);
 
         if (GestureInfo != null)
         {
@@ -143,53 +250,85 @@ public class GestureListener : MonoBehaviour, KinectGestures.GestureListenerInte
         if (gesture == KinectGestures.Gestures.Musibidachi)
         {
             Musibidachi = true;
-            contador++;
+            contador1++;
         }
         else if (gesture == KinectGestures.Gestures.Joe)
         {
-            Joe= true;
-            contador++;
+            Joe = true;
+            contador2++;
         }
         else if (gesture == KinectGestures.Gestures.Zenkutsu)
         {
             Zenkutsu = true;
-            contador++;
+            contador3++;
         }
         else if (gesture == KinectGestures.Gestures.Kokutsu)
         {
             Kokutsu = true;
-            contador++;
+            contador4++;
         }
         else if (gesture == KinectGestures.Gestures.Tsuki)
         {
             Tsuki = true;
-            contador++;
+            contador5++;
         }
         else if (gesture == KinectGestures.Gestures.OiZuki)
         {
             OiZuki = true;
-            contador++;
+            contador6++;
         }
         else if (gesture == KinectGestures.Gestures.Uke)
         {
             Uke = true;
-            contador++;
+            contador7++;
         }
         else if (gesture == KinectGestures.Gestures.AgeUke)
         {
             AgeUke = true;
-            contador++;
+            contador8++;
         }
 
-        if (contador == 3)
+        if (MovActual == 1)
         {
-            GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
+            if (contador1 == 3)
+                GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
+        }
+        else if (MovActual == 2)
+        {
+            if (contador2 == 3)
+                GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
+        }
+        else if (MovActual == 3)
+        {
+            if (contador3 == 3)
+                GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
+        }
+        else if (MovActual == 4)
+        {
+            if (contador4 == 3)
+                GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
+        }
+        else if (MovActual == 5)
+        {
+            if (contador5 == 3)
+                GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
+        }
+        else if (MovActual == 6)
+        {
+            if (contador6 == 3)
+                GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
+        }
+        else if (MovActual == 7)
+        {
+            if (contador7 == 3)
+                GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
+        }
+        else if (MovActual == 8)
+        {
+            if (contador8 == 3)
+                GestureInfo.GetComponent<GUIText>().text = "Siguiente Movimiento";
         }
 
-
-
-
-        Debug.Log(contador);
         return true;
 
     }
